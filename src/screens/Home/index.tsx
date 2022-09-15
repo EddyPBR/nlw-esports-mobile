c1e7816713982ed2fd9eb0c861303d3c1264274e
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-15 14:58:48 -0300

    :sparkles: create Home.Screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { View } from "react-native";
+
+import { styles } from "./styles";
+
+export const Home: FC = () => {
+  return <View style={styles.container}></View>;
+};
