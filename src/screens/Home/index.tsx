44b0151f2e287db12a08798b41a80df3b85947dc
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-15 20:35:54 -0300

    :sparkles: add games list in Home.Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,31 @@
 import { FC } from "react";
-import { View } from "react-native";
+import { View, Image, FlatList } from "react-native";
 
 import { styles } from "./styles";
 
+import logoImg from "~assets/background-galaxy.png";
+import { GameCard } from "~components/GameCard";
+import { Heading } from "~components/Heading";
+import { GAMES } from "~utils/games";
+
 export const Home: FC = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+        horizontal
+      />
+    </View>
+  );
 };
